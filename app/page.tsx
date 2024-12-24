e056b57449498005f6d77ef917ee0962e8d6fa89
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-12-25 04:16:28 +0530

    feat: integrate local fonts for enhanced typography and layout consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-light-500 text-3xl font-black">DevEase</h1>
-      <p>One stop solutions for queries by developers for developers</p>
+      <h1>DevEase</h1>
+      <p className="font-inter text-3xl font-bold">
+        One stop solutions for queries by developers for developers
+      </p>
+      <p className="font-space-grotesk text-3xl font-bold">
+        One stop solutions for queries by developers for developers
+      </p>
     </div>
   );
 }
