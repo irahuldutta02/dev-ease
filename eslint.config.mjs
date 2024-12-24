ab4737bbb9025bf33e864e9a023c6e56ec9690f5
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-12-25 03:25:24 +0530

    chore: update package.json and eslint configuration with new linting rules and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import tailwind from "eslint-plugin-tailwindcss";
+import neostandard from "neostandard";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,45 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  ...neostandard(),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...tailwind.configs["flat/recommended"],
+  {
+    rules: {
+      "@stylistic/quotes": "off",
+      "@stylistic/semi": "off",
+      "@stylistic/space-before-function-paren": "off",
+      "@stylistic/jsx-quotes": "off",
+      camelcase: "off",
+      "no-undef": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Built-in types are first
+            "external", // External libraries
+            "internal", // Internal modules
+            ["parent", "sibling"], // Parent and sibling types can be mingled together
+            "index", // Then the index file
+            "object", // Object imports
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
