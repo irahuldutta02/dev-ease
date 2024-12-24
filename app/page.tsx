49466109fcbd06b1b188fdc31bb7a3f2814827e8
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-12-25 03:25:37 +0530

    style: enhance layout and page title styling for improved presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>DevEase</h1>
+      <h1 className="text-3xl font-black text-white ">DevEase</h1>
       <p>One stop solutions for queries by developers for developers</p>
     </div>
   );
 }
-
