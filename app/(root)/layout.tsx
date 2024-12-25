90408481c44ebbd16b662911db982e9df9431e3e
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-12-25 23:24:08 +0530

    feat: implement authentication layout and sign-in/sign-up pages with Navbar integration

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Navbar from "@/components/navigation/navbar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <main>
+        <Navbar />
+        {children}
+      </main>
+    </>
+  );
+}
