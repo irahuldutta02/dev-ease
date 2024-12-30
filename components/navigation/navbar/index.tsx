66f95fab61ba1f50d20e5fb60dec119672eda736
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-12-30 23:12:19 +0530

    feat: add new pages for Tags, Find Jobs, Community, Profile, Collections, and Ask a Question; implement sidebar links for navigation

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,30 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <>
-      <nav className="flex-between background-light900_dark200 sm:px:12 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none">
-        <Link href="/" className="flex items-center gap-2">
-          <Image
-            src="/images/site-logo.svg"
-            width={23}
-            height={23}
-            alt="DevEase logo"
-          />{" "}
-          <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-            Dev<span className="text-primary-500">Ease</span>
-          </p>
-        </Link>
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="DevFlow Logo"
+        />
 
-        <p>Global Search</p>
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
 
-        <div className="flex-between gap-5">
-          <Theme />
-        </div>
-      </nav>
-    </>
+      <p>Global Search</p>
+
+      <div className="flex-between gap-5">
+        <Theme />
+
+        <MobileNavigation />
+      </div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
